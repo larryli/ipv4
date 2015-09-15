ca1fa2addfd31aad0627f0c1a918a5fe959419a0
Larry Li <larryli@qq.com>
2015-09-15 20:16:35 +0800

    fix yii env
    Signed-off-by: Larry Li <larryli@qq.com>

--- yii2/web/index.php
+++ yii2/web/index.php
@@ -5,9 +5,6 @@
  * Author: Larry Li <larryli@qq.com>
  */
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
 require(dirname(__DIR__) . '/bootstrap.php');
 $config = require(__DIR__ . '/../config/web.php');
 (new yii\web\Application($config))->run();
